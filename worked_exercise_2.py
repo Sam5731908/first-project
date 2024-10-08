4ab178faafae9691533d36b718a038ff1560c938
Sam Smith Smith (s5731908) <s5731908@bournemouth.ac.uk>
2024-10-08 15:33:42 +0100

    hello

---
+++ worked_exercise_2.py
@@ -0,0 +1,35 @@
+a = 11
+b = 5
+result = a < b
+type_result = type(result)
+print(f"the result is {result} and the type is {type_result}")
+
+a = 11
+b = 5
+result = a > b
+type_result = type(result)
+print(f"the result is {result} and the type is {type_result}")
+
+a = 11
+b = 5
+result = a <= b
+type_result = type(result)
+print(f"the result is {result} and the type is {type_result}")
+
+a = 11
+b = 5
+result = a >= b
+type_result = type(result)
+print(f"the result is {result} and the type is {type_result}")
+
+a = 11
+b = 5
+result = a != b
+type_result = type(result)
+print(f"the result is {result} and the type is {type_result}")
+
+a = 11
+b = 5
+result = a == b
+type_result = type(result)
+print(f"the result is {result} and the type is {type_result}")
---
+++ worked_exercise_3.py
@@ -0,0 +1,6 @@
+greeting_string = "welcome to programming unit! This is level 4 unit :)"
+print("The first character is",greeting_string[0])
+print("The fith character is",greeting_string[4])
+print("The first 5 characters are",greeting_string[0:5])
+print("The last character is",greeting_string[-1])
+print("The last 4 characters are",greeting_string[-4:])
